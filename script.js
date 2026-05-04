b6dc293f980dc4e2127c4d3db579b846fc7663cc
otavioapc <otavioaugcampos08@gmail.com>
2026-05-04 18:18:48 -0300

    feat: scroll suave do site

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const ano = document.getElementById("ano");
 
 //Preenchendo ano atual
 ano.textContent = new Date().getFullYear();
+
+//Tirar links internaos da url durante scroll
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        const id = link.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    });
+});
